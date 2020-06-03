d1a35ca47b3a63d6ed34f1d73588b1013e8a7d34
Olzumzum <evdokimova_olga_15@mail.ru>
2020-06-03 17:01:02 +0700

    график масштабируется, пытаюсь настроить отображение дат для оси Х

--- app/src/main/java/com/example/myperformance/presenters/DailyProductivity.java
+++ app/src/main/java/com/example/myperformance/presenters/DailyProductivity.java
@@ -5,39 +5,44 @@ import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.Shader;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.androidplot.Plot;
 import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.PanZoom;
 import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.StepMode;
 import com.androidplot.xy.XYGraphWidget;
 import com.androidplot.xy.XYPlot;
 import com.androidplot.xy.XYSeries;
+import com.androidplot.xy.ZoomEstimator;
 import com.example.myperformance.R;
 import com.example.myperformance.workCharts.ChartDataHolder;
 import com.example.myperformance.workCharts.ReturningDataChart;
 
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DailyProductivity extends AppCompatActivity {
 
-    private List<Integer> keyDate = new ArrayList<>();
+    private List<Double> keyDate = new ArrayList<>();
     private List<Integer> valueTime = new ArrayList<>();
-    private static final String MONTH_TITLE_SERIAS = "Производительность за несколько дней";
+    private static final String MONTH_TITLE_SERIAS = "Производительность";
+    PanZoom panZoom;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.daily_productivity);
+        LinearLayout plotLayout = findViewById(R.id.plot_layout);
+//        plotLayout.setOnTouchListener((View.OnTouchListener) this);
 
         XYPlot plot = findViewById(R.id.plot);
-        plot.setPlotMargins(0, 0, 0, 0);
-        plot.setPlotPadding(0, 0, 0, 0);
-
 
         //получить данные для отображения на графике
         getData();
@@ -71,10 +76,19 @@ public class DailyProductivity extends AppCompatActivity {
         //Range
         //настройка шага по осям (чтобы измерения были не десятичными, а целыми)
         plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.LEFT).setFormat(new DecimalFormat("0"));
-        plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new DecimalFormat("0"));
+        plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new SimpleDateFormat("dd.MM"));
+        //установить начало оси Х
+//        plot.setUserDomainOrigin(keyDate.get(0));
+        plot.setBorderStyle(Plot.BorderStyle.NONE, null, null);
 
+        plot.setRangeLabel("");
+        plot.setDomainLabel("");
 
         plot.addSeries(series1, series1Format);
+
+        panZoom = PanZoom.attach(plot, PanZoom.Pan.BOTH, PanZoom.Zoom.STRETCH_BOTH, PanZoom.ZoomLimit.MIN_TICKS);
+        plot.getRegistry().setEstimator(new ZoomEstimator());
+        plot.getOuterLimits().set(0, 3000, 0, 1000);
 
     }
 
--- app/src/main/java/com/example/myperformance/workCharts/ChartDataHolder.java
+++ app/src/main/java/com/example/myperformance/workCharts/ChartDataHolder.java
@@ -1,5 +1,7 @@
 package com.example.myperformance.workCharts;
 
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -9,26 +11,51 @@ import java.util.TreeMap;
 
 public class ChartDataHolder implements ReturningDataChart {
     private Map<Calendar, Integer> timeHolderMap = new TreeMap<>();
-    private List<Integer> keyDate = new ArrayList<>();
+    private List<Double> keyDate = new ArrayList<>();
     private List<Integer> valueTime = new ArrayList<>();
 
-    public ChartDataHolder(){
-        timeHolderMap.put(new GregorianCalendar(2006, 0, 15), 15);
-        timeHolderMap.put(new GregorianCalendar(2006, 0, 16), 4);
-        timeHolderMap.put(new GregorianCalendar(2006, 0, 17), 20);
+    public ChartDataHolder() {
+        timeHolderMap.put(new GregorianCalendar(2006, 5, 15), 15);
+        timeHolderMap.put(new GregorianCalendar(2006, 5, 16), 4);
+        timeHolderMap.put(new GregorianCalendar(2006, 5, 17), 20);
         timeHolderMap.put(new GregorianCalendar(2006, 0, 18), 0);
         timeHolderMap.put(new GregorianCalendar(2006, 0, 19), 3);
+        timeHolderMap.put(new GregorianCalendar(2006, 0, 20), 3);
+        timeHolderMap.put(new GregorianCalendar(2006, 0, 21), 3);
+        timeHolderMap.put(new GregorianCalendar(2006, 0, 22), 5);
+        timeHolderMap.put(new GregorianCalendar(2006, 0, 23), 8);
+        timeHolderMap.put(new GregorianCalendar(2006, 11, 24), 3);
+//        timeHolderMap.put(new GregorianCalendar(2006, 0, 25), 3);
+//        timeHolderMap.put(new GregorianCalendar(2006, 0, 26), 3);
+//        timeHolderMap.put(new GregorianCalendar(2006, 0, 27), 3);
+//        timeHolderMap.put(new GregorianCalendar(2006, 0, 28), 3);
+//        timeHolderMap.put(new GregorianCalendar(2006, 0, 29), 3);
+//        timeHolderMap.put(new GregorianCalendar(2006, 0, 30), 3);
+//        timeHolderMap.put(new GregorianCalendar(2006, 0, 31), 3);
+//        timeHolderMap.put(new GregorianCalendar(2006, 1, 1), 3);
+//        timeHolderMap.put(new GregorianCalendar(2006, 1, 2), 3);
+//        timeHolderMap.put(new GregorianCalendar(2006, 1, 3), 3);
+//        timeHolderMap.put(new GregorianCalendar(2006, 1, 4), 3);
+//        timeHolderMap.put(new GregorianCalendar(2006, 1, 5), 3);
+//        timeHolderMap.put(new GregorianCalendar(2006, 1, 6), 3);
+//        timeHolderMap.put(new GregorianCalendar(2006, 1, 7), 3);
+//        timeHolderMap.put(new GregorianCalendar(2006, 1, 8), 3);
 
         timeAndDateRecording();
     }
 
     //запись данных о датах и времени в списки для отображения в виде графика
-    private void timeAndDateRecording(){
-        for(Map.Entry entry : timeHolderMap.entrySet()){
+    private void timeAndDateRecording() {
+        for (Map.Entry entry : timeHolderMap.entrySet()) {
             //получаем дату - ключ
             Calendar infoDate = (Calendar) entry.getKey();
             //записываем в список
-            keyDate.add(infoDate.get(Calendar.DAY_OF_MONTH));
+
+            Log.d("DaysMap", "день " + infoDate.get(Calendar.DAY_OF_MONTH) + "."
+                    + infoDate.get(Calendar.MONTH) + " время " + entry.getValue());
+            double date = infoDate.get(Calendar.MONTH);
+            date += infoDate.get(Calendar.DAY_OF_MONTH);
+            keyDate.add(date);
             //записываем в список данные о времени
             valueTime.add((Integer) entry.getValue());
 
@@ -41,7 +68,7 @@ public class ChartDataHolder implements ReturningDataChart {
     }
 
     @Override
-    public List<Integer> getListDayOfWeek() {
+    public List<Double> getListDayOfWeek() {
         return keyDate;
     }
 
