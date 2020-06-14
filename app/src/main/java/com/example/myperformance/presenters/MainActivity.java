d1a5b1b7849bc706222de37bc6cd3e25b509fa34
Olzumzum <evdokimova_olga_15@mail.ru>
2020-06-14 17:22:45 +0700

    added a temporary button to move between activities

--- app/src/main/java/com/example/myperformance/presenters/MainActivity.java
+++ app/src/main/java/com/example/myperformance/presenters/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myperformance.presenters;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -58,6 +59,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 coutingTime.pauseCounting();
+            }
+        });
+
+        Button grathact = findViewById(R.id.grath_activity);
+        grathact.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DailyProductivity.class );
+                startActivity(intent);
             }
         });
     }
