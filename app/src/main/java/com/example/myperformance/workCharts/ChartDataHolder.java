6698ba45b013f37fa70eb9d7696387bdf2948120
Olzumzum <evdokimova_olga_15@mail.ru>
2020-06-18 16:24:56 +0700

    chartDataHolder comments

--- app/src/main/java/com/example/myperformance/workCharts/ChartDataHolder.java
+++ app/src/main/java/com/example/myperformance/workCharts/ChartDataHolder.java
@@ -9,46 +9,23 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+/**
+ * Prepares data for presentation on a chart
+ * @param <E>
+ */
 public class ChartDataHolder <E> implements ReturningDataChart {
-//    private Map<Calendar, Integer> timeHolderMap = new TreeMap<>();
-    private List<E> keyDate = new ArrayList<>();
+
+    //list of dates on which the count was made
+    private List<E> valueDate = new ArrayList<>();
+    //time count list
     private List<Integer> valueTime = new ArrayList<>();
 
-
-
     public ChartDataHolder(List<E> list) {
-//        timeHolderMap.put(new GregorianCalendar(2006, 5, 15), 15);
-//        timeHolderMap.put(new GregorianCalendar(2006, 5, 16), 4);
-//        timeHolderMap.put(new GregorianCalendar(2006, 5, 17), 20);
-//        timeHolderMap.put(new GregorianCalendar(2006, 0, 18), 0);
-//        timeHolderMap.put(new GregorianCalendar(2006, 0, 19), 3);
-//        timeHolderMap.put(new GregorianCalendar(2006, 0, 20), 3);
-//        timeHolderMap.put(new GregorianCalendar(2006, 0, 21), 3);
-//        timeHolderMap.put(new GregorianCalendar(2006, 0, 22), 5);
-//        timeHolderMap.put(new GregorianCalendar(2006, 0, 23), 8);
-//        timeHolderMap.put(new GregorianCalendar(2006, 11, 24), 3);
-//        timeHolderMap.put(new GregorianCalendar(2006, 0, 25), 3);
-//        timeHolderMap.put(new GregorianCalendar(2006, 0, 26), 3);
-//        timeHolderMap.put(new GregorianCalendar(2006, 0, 27), 3);
-//        timeHolderMap.put(new GregorianCalendar(2006, 0, 28), 3);
-//        timeHolderMap.put(new GregorianCalendar(2006, 0, 29), 3);
-//        timeHolderMap.put(new GregorianCalendar(2006, 0, 30), 3);
-//        timeHolderMap.put(new GregorianCalendar(2006, 0, 31), 3);
-//        timeHolderMap.put(new GregorianCalendar(2006, 1, 1), 3);
-//        timeHolderMap.put(new GregorianCalendar(2006, 1, 2), 3);
-//        timeHolderMap.put(new GregorianCalendar(2006, 1, 3), 3);
-//        timeHolderMap.put(new GregorianCalendar(2006, 1, 4), 3);
-//        timeHolderMap.put(new GregorianCalendar(2006, 1, 5), 3);
-//        timeHolderMap.put(new GregorianCalendar(2006, 1, 6), 3);
-//        timeHolderMap.put(new GregorianCalendar(2006, 1, 7), 3);
-//        timeHolderMap.put(new GregorianCalendar(2006, 1, 8), 3);
-
-
         timeAndDateRecording(list);
     }
 
     /**
-     * запись данных о датах и времени в списки для отображения в виде графика
+     * writing date and time data to lists for display in a graph
      */
     private void timeAndDateRecording(List<E> listValue) {
 
@@ -62,29 +39,23 @@ public class ChartDataHolder <E> implements ReturningDataChart {
                     + infoDate.get(Calendar.MONTH) + " время " + el.getTimePerf());
 
 
-            keyDate.add((E) infoDate.getTime());
+            valueDate.add((E) infoDate.getTime());
             //записываем в список данные о времени
             valueTime.add(el.getTimePerf());
-
-
-//            Log.d("MyLog", "Текущая дата и колчество часов " +
-//                    ((Calendar) entry.getKey()).get(Calendar.DAY_OF_MONTH) +
-//                    " " +
-//                    entry.getValue());
         }
     }
 
     /**
-     * @return вернуть список дней
+     * @return list of days
      */
     @Override
     public  List<?> getListDayOfWeek() {
-        return keyDate;
+        return valueDate;
     }
 
     /**
      *
-     * @return вернуть список значений времени
+     * @return time value list
      */
     @Override
     public List<Integer> getListTimeValue() {
