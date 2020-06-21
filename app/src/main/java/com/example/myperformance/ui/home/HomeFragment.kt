45ca00c5f22dd1639f1652e0f2c7ba6d10e6c66a
Olzumzum <evdokimova_olga_15@mail.ru>
2020-06-21 18:25:48 +0700

    fragment timer counting

--- app/src/main/java/com/example/myperformance/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/myperformance/ui/home/HomeFragment.kt
@@ -1,31 +1,80 @@
 package com.example.myperformance.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Chronometer
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.myperformance.R
+import com.example.myperformance.presenters.DailyProductivityActivity
+import com.example.myperformance.worktime.CoutingTime
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), View.OnClickListener {
+    private val FLAG_TIMER_COUNT = "TimerCount"
+    private val FLAG_RUNNING_TIMER = "RunningTimer"
 
-    private lateinit var homeViewModel: HomeViewModel
+    //provides a time
+    lateinit var coutingTime: CoutingTime
+
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+
         return root
     }
+
+//    override fun onActivityCreated(savedInstanceState: Bundle?) {
+//        super.onActivityCreated(savedInstanceState)
+//
+//        val startCoutingTime = findViewById<Button>(R.id.start_countring_time)
+//        val pauseCoutingTime = container?.findViewById<Button>(R.id.pause_countring_time)
+//        val stopCoutingTime = container?.findViewById<Button>(R.id.stop_countring_time)
+//
+//        val chronometerEmployment = container?.findViewById<Chronometer>(R.id.chronometer_employment)
+//
+//        coutingTime = CoutingTime(chronometerEmployment)
+//
+//        //restore state
+//        //get time counter value
+//        savedInstanceState.let {
+//            val pauseOffset = savedInstanceState?.getLong(FLAG_TIMER_COUNT)
+//            val running = savedInstanceState?.getBoolean(FLAG_RUNNING_TIMER)
+//            savedInstanceState?.putLong(FLAG_TIMER_COUNT, 0)
+//            if (pauseOffset != null) {
+//                coutingTime.pauseOffset = pauseOffset
+//            }
+//
+//            if (running != null) {
+//                coutingTime.running = running
+//            }
+//
+//            coutingTime.restartChronometr()
+//        }
+//
+//        startCoutingTime?.setOnClickListener(this)
+//        pauseCoutingTime?.setOnClickListener(this)
+//        stopCoutingTime?.setOnClickListener(this)
+//
+//
+//    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.start_countring_time -> coutingTime.startCount()
+            R.id.pause_countring_time -> coutingTime.pauseCounting()
+            R.id.stop_countring_time -> coutingTime.stopCounting()
+        }
+    }
+
 }
