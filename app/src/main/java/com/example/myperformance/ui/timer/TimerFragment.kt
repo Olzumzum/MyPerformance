cc7dd3d18dfc16c9227b189fc0b93b3efae12902
Olzumzum <evdokimova_olga_15@mail.ru>
2020-06-22 15:07:52 +0700

    labels changes

--- app/src/main/java/com/example/myperformance/ui/timer/TimerFragment.kt
+++ app/src/main/java/com/example/myperformance/ui/timer/TimerFragment.kt
@@ -1,21 +1,14 @@
-package com.example.myperformance.ui.home
+package com.example.myperformance.ui.timer
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.Chronometer
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.example.myperformance.R
-import com.example.myperformance.presenters.DailyProductivityActivity
 import com.example.myperformance.worktime.CoutingTime
 
-class HomeFragment : Fragment(), View.OnClickListener {
+class TimerFragment : Fragment(), View.OnClickListener {
     private val FLAG_TIMER_COUNT = "TimerCount"
     private val FLAG_RUNNING_TIMER = "RunningTimer"
 
@@ -28,7 +21,7 @@ class HomeFragment : Fragment(), View.OnClickListener {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val root = inflater.inflate(R.layout.fragment_timer, container, false)
 
 
         return root
