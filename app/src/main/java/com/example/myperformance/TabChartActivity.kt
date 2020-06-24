fb88af664e8410137358a65d834ddd811238e67a
Olzumzum <evdokimova_olga_15@mail.ru>
2020-06-24 16:31:34 +0700

    add tab activity

---
+++ app/src/main/java/com/example/myperformance/TabChartActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myperformance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TabChartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tab_chart)
+    }
+}
