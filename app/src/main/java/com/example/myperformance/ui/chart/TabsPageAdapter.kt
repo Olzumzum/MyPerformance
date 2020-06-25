f077fbb96bdde1d2ef9b8188d33e15c8a6f77c7e
Olzumzum <evdokimova_olga_15@mail.ru>
2020-06-25 19:20:11 +0700

    working tabs

--- app/src/main/java/com/example/myperformance/ui/chart/TabsPageAdapter.kt
+++ app/src/main/java/com/example/myperformance/ui/chart/TabsPageAdapter.kt
@@ -1,30 +1,22 @@
 package com.example.myperformance.ui.chart
 
-import android.util.Log
+
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 import com.example.myperformance.EmptyFragment1
 import com.example.myperformance.EmptyFragment2
 import com.example.myperformance.EmptyFragment3
-import java.lang.NullPointerException
+import java.lang.Exception
+
 
 class TabsPageAdapter(fm: FragmentManager, private val countItems: Int): FragmentStatePagerAdapter(fm) {
     override fun getItem(position: Int): Fragment {
         when(position){
-            0 -> {
-                Log.d("MyLog", "Empty Fragment 1")
-                return EmptyFragment1()
-            }
-            1 -> {
-                Log.d("MyLog", "Empty Fragment 2")
-                return EmptyFragment2()
-            }
-            3 -> {
-                Log.d("MyLog", "Empty Fragment 3")
-                return EmptyFragment3()
-            }
-            else -> throw NullPointerException()
+            0 -> return EmptyFragment1()
+            1 -> return EmptyFragment2()
+            2 -> return EmptyFragment3()
+            else -> throw Exception("Error in selection of fragment")
         }
     }
 
