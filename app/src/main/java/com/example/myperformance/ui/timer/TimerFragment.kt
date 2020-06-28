adf337de28db4a925a11d27f01778e67a4971a53
Olzumzum <evdokimova_olga_15@mail.ru>
2020-06-28 19:20:56 +0700

    very strange button animation

--- app/src/main/java/com/example/myperformance/ui/timer/TimerFragment.kt
+++ app/src/main/java/com/example/myperformance/ui/timer/TimerFragment.kt
@@ -1,22 +1,32 @@
 package com.example.myperformance.ui.timer
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
+import android.content.Context
 import android.content.res.Configuration
+import android.graphics.Color
+import android.graphics.drawable.TransitionDrawable
+import android.os.Build
 import android.os.Bundle
+import android.provider.CalendarContract
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.Chronometer
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.myperformance.R
 import com.example.myperformance.worktime.CoutingTime
 
-class TimerFragment : Fragment(), View.OnClickListener {
+class TimerFragment : Fragment(), View.OnClickListener, View.OnTouchListener {
     private val FLAG_TIMER_COUNT = "TimerCount"
     private val FLAG_RUNNING_TIMER = "RunningTimer"
 
     private var viewRoot: View? = null
+    lateinit var startCoutingTime: Button
 
     //provides a time
     lateinit var coutingTime: CoutingTime
@@ -31,7 +41,7 @@ class TimerFragment : Fragment(), View.OnClickListener {
 
         if (orientation == Configuration.ORIENTATION_PORTRAIT)
             viewRoot = inflater.inflate(R.layout.fragment_timer, container, false)
-        if(orientation == Configuration.ORIENTATION_LANDSCAPE)
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE)
             viewRoot = inflater.inflate(R.layout.fragment_timer_horizontal, container, false)
 
 
@@ -41,7 +51,7 @@ class TimerFragment : Fragment(), View.OnClickListener {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        val startCoutingTime = viewRoot?.findViewById<Button>(R.id.start_countring_time)
+        startCoutingTime = viewRoot?.findViewById<Button>(R.id.start_countring_time)!!
         val pauseCoutingTime = viewRoot?.findViewById<Button>(R.id.pause_countring_time)
         val stopCoutingTime = viewRoot?.findViewById<Button>(R.id.stop_countring_time)
 
@@ -66,19 +76,60 @@ class TimerFragment : Fragment(), View.OnClickListener {
             coutingTime.restartChronometr()
         }
 
-        startCoutingTime?.setOnClickListener(this)
+        this.startCoutingTime.setOnClickListener(this)
         pauseCoutingTime?.setOnClickListener(this)
         stopCoutingTime?.setOnClickListener(this)
 
 
     }
 
+    private fun colorize(v: View) {
+        val c = context?.let { ContextCompat.getColor(it, R.color.button_pressure) }
+        val k = context?.let { ContextCompat.getColor(it, R.color.MyPerfDark) }
+
+        if (c != null && k != null) {
+            val animator = ObjectAnimator.ofInt(v, "backgroundColor",
+                    c, k)
+            animator.duration = 200
+            animator.repeatCount = 1
+            animator.repeatMode = ObjectAnimator.REVERSE
+//        animator.disableViewDuringAnimation(startCoutingTime)
+            animator.start()
+        }
+        Log.d("MyLog", "Мы были в анимации")
+    }
+
+    private fun ObjectAnimator.disableViewDuringAnimation(view: View) {
+        addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationStart(animation: Animator?) {
+                view.isEnabled = false
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                view.isEnabled = true
+            }
+        })
+    }
+
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.start_countring_time -> coutingTime.startCount()
-            R.id.pause_countring_time -> coutingTime.pauseCounting()
-            R.id.stop_countring_time -> coutingTime.stopCounting()
+            R.id.start_countring_time -> {
+                coutingTime.startCount()
+                colorize(v)
+
+            }
+            R.id.pause_countring_time -> {
+
+                coutingTime.pauseCounting()
+                colorize(v)
+            }
+            R.id.stop_countring_time -> {
+                coutingTime.stopCounting()
+                colorize(v)
+            }
         }
+
+
     }
 
     override fun onPause() {
@@ -93,6 +144,25 @@ class TimerFragment : Fragment(), View.OnClickListener {
         outState.putLong(FLAG_TIMER_COUNT, coutingTime.pauseOffset)
         outState.putBoolean(FLAG_RUNNING_TIMER, coutingTime.running)
         super.onSaveInstanceState(outState)
+    }
+
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        val scaleUp = AnimationUtils.loadAnimation(this.activity, R.anim.button_up)
+        val scaleDown = AnimationUtils.loadAnimation(this.activity, R.anim.button_down)
+
+        when (event?.action) {
+            MotionEvent.ACTION_UP -> {
+                v?.startAnimation(scaleUp)
+
+
+            }
+            MotionEvent.ACTION_BUTTON_RELEASE -> {
+                v?.startAnimation(scaleDown)
+
+            }
+        }
+        return true
     }
 
 
